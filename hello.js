fdf2925a7733871bffdc1e8128bd51c159d18941
SAUMYA NEUPANE <105053199+Saumyaaaaa@users.noreply.github.com>
2024-02-05 16:13:52 +0545

    nodemon is added

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
 const {add,sub}=require ("./script");
-console.log("MAth VAlue is",add(2,4));
 
-console.log("Math Value is", sub(2, 4));
+console.log("Math Value is", sub(8, 4));
+console.log("MAth VAlue is",add(2,4));
